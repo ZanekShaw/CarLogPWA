bd50ebc0bde6711fe69047a1e17cc4c1f09021e7
Zanek Shaw <zaneks123@gmail.com>
2024-11-06 21:05:30 +1100

    5

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,12 +9,10 @@ const nextConfig = {
   },
 };
 
-const withPWA = ({
+export default withPWA({
+  ...nextConfig,
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
 });
-
-
-module.exports = withPWA(nextConfig);
