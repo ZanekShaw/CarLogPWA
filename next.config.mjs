356b92cc86caac771242bb51b8500a5e24167290
Zanek Shaw <zaneks123@gmail.com>
2024-11-06 21:41:19 +1100

    7

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
-  swSrc: "pages/sw.ts",
+  swSrc: "/sw.ts",
   swDest: "public/sw.js",
 });
 
 export default withSerwist({
-  // Your Next.js config
+      reactStrictMode: true
 });
